b8d16e5804846f23fec1587a59fe1cc0d89f26d4
Josh Weisman <josh.m.weisman@gmail.com>
2021-06-06 18:19:17 +0300

    Rename primo folder

--- primo-studio-appointment-scheduler/src/index.js
+++
@@ -1,8 +0,0 @@
-import { appointmentScheduler } from './module';
-
-/* Required for modules used by Primo Studio */
-try {
-  if (app && app.requires) {
-    app.requires.push(appointmentScheduler.name);
-  }
-} catch(e) { }
---
+++ primo/src/environment.js
@@ -0,0 +1,4 @@
+export const environment = {
+  /* schedulerApi: "https://api-eu.exldevnetwork.net/alma-scheduler/patron", */
+  schedulerApi: "http://localhost:3000/patron"
+}
