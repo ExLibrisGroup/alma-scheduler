13ff82df4b8ef3d447227bb7a07af8f4dd86af41
Josh Weisman <josh.m.weisman@gmail.com>
2021-06-06 18:20:00 +0300

    Update environment.js

--- primo/src/environment.js
+++ primo/src/environment.js
@@ -1,4 +1,3 @@
 export const environment = {
-  /* schedulerApi: "https://api-eu.exldevnetwork.net/alma-scheduler/patron", */
-  schedulerApi: "http://localhost:3000/patron"
+  schedulerApi: "https://api-eu.exldevnetwork.net/alma-scheduler/patron"
 }
